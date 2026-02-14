cf5d88895de8d0d8c1b1c4a14e68eb58f04f4cb5
Rakshit Yadav <rakshityadav@Rakshits-MacBook-Air.local>
2026-02-14 11:09:32 +0530

    first commit

---
+++ js day 7/sc.js
@@ -0,0 +1,5 @@
+const para = document.createElement("p");
+para.textContent="This is a dt=ynamically added para";
+console.log(para);
+
+
---
+++ task1/sc.js
@@ -0,0 +1,19 @@
+const profile = document.getElementById
+("profile");
+
+profile.style.background="tomato";
+profile.style.padding="15px";
+profile.style.textAlign="center"
+
+
+//task2
+
+const highlight = Document.
+getElementsByClassName("important");
+console.log(highlight)
+
+for(let i=0;i<x.length;i++){
+
+    x[i].style.color="red"
+}
+
